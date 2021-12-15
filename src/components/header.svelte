9808d85a64082d611e0fb799bcc1547fb924236a
cajukev <kcajukev@gmail.com>
2021-12-15 12:40:29 -0500

    Small changes

---
+++ src/components/header.svelte
@@ -0,0 +1,26 @@
+<header class="">
+	<div id="logo" class="grid grid-rows-2 sm:grid sm:grid-rows-1 sm:grid-cols-[1fr,3fr]">
+		<svg class="" viewBox="0 0 128 89" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M2.56051 86.3107L7.21525 86.3281V19.2297H1.45532V2.4552H22.5128V86.328H99.0799C131.498 85.5591 136.524 40.8083 99.0799 40.7861C120.553 40.044 119.668 2.60677 99.0799 2.4552H76.4364V78.3582H53.4901L30.1616 40.7861L53.4901 2.4552H68.4052L45.459 40.7861L60.8011 65.7708"
+				stroke="#0400C7"
+				stroke-width="5"
+				stroke-dasharray="688.246 20"
+			/>
+		</svg>
+	</div>
+  <div class="text-center">
+    <p class="font-semibold">
+      Kevin Brousseau
+    </p>
+    <p class="font-semibold">
+      Bachelier en Sciences Informatiques
+    </p>
+    <p>
+      Expert en sites web à la recherche d'un post à temps plein.
+    </p>
+  </div>
+</header>
+
+<style lang="postcss">
+</style>
