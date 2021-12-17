0e472c5089906b0248ae9bd702cc799bb948792a
cajukev <kcajukev@gmail.com>
2021-12-17 18:27:07 -0500

    video test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,12 @@
 <script>
+  import '@slightlyoff/lite-vimeo';
+
 </script>
 
 <section id="APropos" class="h-screen">
   <p class="">A Propos</p>
   <h1>Je crée des sites web, de la conception au développement</h1>
-  <iframe title="video1" src="https://player.vimeo.com/video/658006740?h=e1d5f7a796" width="640" height="900" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+  <lite-vimeo videoid="364402896"></lite-vimeo>
 </section>
 <section id="Solutions" class="h-screen">
   <p>Solutions</p>
