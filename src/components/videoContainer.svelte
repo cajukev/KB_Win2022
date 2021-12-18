c23a5e0b9bbb9c4ed81570a4ca797316835c0e5a
cajukev <kcajukev@gmail.com>
2021-12-18 03:47:46 -0500

    Aspect Ratio cls test

--- src/components/videoContainer.svelte
+++ src/components/videoContainer.svelte
@@ -11,8 +11,8 @@
 </script>
 
 <div class="grid " on:mouseover={swap} on:focus={swap}  >
-	<video bind:this={video} controls src="video{num}.mkv" class="row-start-1 row-span-1 col-start-1 col-span-1 w-full z-0 "/>
-	<picture bind:this={thumbnail} id="thumbnail" class="row-start-1 row-span-1 col-start-1 col-span-1 z-10 w-full ">
+	<video bind:this={video} controls src="video{num}.mkv" class="row-start-1 row-span-1 col-start-1 col-span-1 w-full z-0 aspect-[37/54]"/>
+	<picture bind:this={thumbnail} id="thumbnail" class="row-start-1 row-span-1 col-start-1 col-span-1 z-10 w-full aspect-h-1 aspect-[37/54]">
 		<source
 			srcset="thumbnails/thumbnail{num}-400.webp"
 			media="(max-width:400px)"
