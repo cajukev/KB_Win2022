d7f123735cc360eae5b084a9b327a69958ac664c
cajukev <kcajukev@gmail.com>
2022-01-10 20:21:18 -0500

    Succes Amélioré

--- src/routes/succes.svelte
+++ src/routes/succes.svelte
@@ -26,14 +26,14 @@
 <section
 	id="Succes"
 	class="
-mt-8 bg-zinc-50 pt-4 shadow-lg shadow-shadowColor flex flex-col items-center 
+my-4 py-4 bg-zinc-50 pt-4 shadow-lg shadow-shadowColor flex flex-col items-center 
 xl:block 
 2xl:bg-transparent 2xl:shadow-none "
 >
 
 <h1
-		class="section-splash mt-2 px-4  text-center text-[clamp(20px,2vw,32px)] 
-		xl:col-span-2 xl:mt-8
+		class="section-splash px-4  text-center text-[clamp(20px,2vw,32px)] 
+		xl:col-span-2 
     2xl:col-span-1 2xl:text-left 2xl:px-0 2xl:max-w-[60%]"
 	>
 		<span>Félicitations!</span> Vous avez enregistré un rendez-vous.
@@ -41,15 +41,15 @@ xl:block
 
 	<p
 		class="section-splash mt-2 px-4  text-center text-italic text-slate-700 text-[clamp(16px,1.6vw,24px)] 
-		xl:col-span-2 xl:mt-8
+		xl:col-span-2 
     2xl:col-span-1 2xl:text-left 2xl:px-0 2xl:max-w-[60%]"
 	>
-		{dateDay + " " + mois[dateMth-1] + " "+ dateYr + " - "+ dateTime + "pm"}
+		{dateDay + " " + mois[dateMth-1] + " "+ dateYr + " - "+ dateTime + (parseInt(dateTime)>11?" pm":' am')}
 </p>
 
 	<p
-		class="section-splash mt-2 px-4  text-center text-italic text-slate-700 text-[clamp(16px,1.6vw,24px)] 
-		xl:col-span-2 xl:mt-8
+		class="section-splash my-2 px-4  text-center text-italic text-slate-700 text-[clamp(16px,1.6vw,24px)] 
+		xl:col-span-2 
     2xl:col-span-1 2xl:text-left 2xl:px-0 2xl:max-w-[60%]"
 	>
 		Attendez-vous à recevoir un appel (514-550-7051) ou un email (kevin.brousseau98@gmail.com).
